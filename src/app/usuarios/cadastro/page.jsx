0b2dd98635824ab3637f351bf0afc57b51a89238
Daniicalil <159494730+Daniicalil@users.noreply.github.com>
2024-08-05 20:38:31 -0300

    aula 05-08

---
+++ src/app/usuarios/cadastro/page.jsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+// import styles from "./page.module.css";
+
+export default function cadastro() {
+  return (
+    <div className='container'>
+      <div>
+        <h1>Cadastro de usuário</h1>
+      </div>
+    </div>
+  );
+}
